334470c52b81bc432a6ec93b9546ba1c85130a33
sgonzalez-2022140 <sgonzalez-2022140@kinal.edu.gt>
2024-03-03 23:53:22 -0600

    Se elimino anterior rama

--- PractivaSupervisada/index.js
+++
@@ -1,7 +0,0 @@
-import { initServer } from "./configs/app.js"
-import { connect } from "./configs/mongo.js"
-import { userDefect } from '../PractivaSupervisada/src/user/user.controller.js'
-
-userDefect()
-initServer()
-connect()
